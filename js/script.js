354b96015903d3019466bfb66235ab206baf606f
Agnieszka Szymlak <agniepiech@gmail.com>
2019-08-05 22:34:37 +0200

    Change index.html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var tweetLink = 'https://twitter.com/intent/tweet?text=';
-var quoteUrl = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+var quoteUrl = 'https://quotesondesign.com/wp-json/postsfilter[orderby]=rand&filter[posts_per_page]=1';
 
 
 function getQuote() {
